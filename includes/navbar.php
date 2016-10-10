e0cba59c38b269f2a0249571d6a11db7c860b6c6
Enari <Spam@minit.nu>
2016-10-10 13:03:18 +0200

    Included navbar in index, register and login

--- includes/navbar.php
+++ includes/navbar.php
@@ -5,8 +5,15 @@
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="login.php">Sign In</a></li>
-				<li><a href="register.php">Sign Up</a></li>
+			<?php
+				if(isset($_SESSION["username"]))
+					echo "<li><a href=\"logout.php\">Logout</a></li>";
+				else
+				{
+					echo "<li><a href=\"login.php\">Sign In</a></li>";
+					echo "<li><a href=\"register.php\">Sign Up</a></li>";
+				}
+			?>
 			</ul>
 		</div>
 	</div>
