c7b25a2ecba4a1972ad4e8872c80dfa5380e3339
Enari <Spam@minit.nu>
2016-10-12 11:08:26 +0200

    Tried to fix navbar comapse, failed

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,11 @@
 		<nav class="navbar navbar-default navbar-static-top">
 			<div class="container">
 				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="navbar">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span> 
+					</button>
 					<a href="index.php" class="navbar-brand">Forum</a>
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
@@ -10,8 +15,8 @@
 							echo "<li><a href=\"logout.php\">Logout</a></li>";
 						else
 						{
-							echo "<li><a href=\"login.php\">Sign In</a></li>";
-							echo "<li><a href=\"register.php\">Sign Up</a></li>";
+							echo "<li><a href=\"login.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Sign In</a></li>";
+							echo "<li><a href=\"register.php\"><span class=\"glyphicon glyphicon-user\"></span> Sign Up</a></li>";
 						}
 					?>
 					
