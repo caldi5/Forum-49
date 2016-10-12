eae9e1341abd28ae784f64677a1f8bc594045887
Enari <Spam@minit.nu>
2016-10-12 12:30:20 +0200

    use bootstrap min instead

--- includes/standard_head.php
+++ includes/standard_head.php
@@ -2,7 +2,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="img/favicon.ico">
-		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/custom.css" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
