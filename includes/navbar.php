7240c9ae85971d9cc6d88c5f51e5a503c9d2d112
Enari <Spam@minit.nu>
2016-10-12 13:30:18 +0200

    Navbar with dropdown

--- includes/navbar.php
+++ includes/navbar.php
@@ -10,16 +10,27 @@
 				</div>
 				<div id="navbar" class="navbar-collapse collapse">
 					<ul class="nav navbar-nav navbar-right">
-					<?php
-						if(isset($_SESSION["username"]))
-							echo "<li><a href=\"logout.php\">Logout</a></li>";
+<?php 			if(isset($_SESSION["username"]))
+				{
+?>
+							<li class="dropdown">
+								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+								<span class="glyphicon glyphicon-user"></span> <?php echo $_SESSION["username"]; ?> <span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<?php echo "<li><a href=\"#\">Profile</a></li>"; ?>
+									<?php echo "<li><a href=\"#\">Settings</a></li>"; ?>
+									<?php echo "<li><a href=\"logout.php\"><span class=\"glyphicon glyphicon-log-out\"></span> Logout</a></li>"; ?>
+
+								</ul>
+							  </li>
+<?php	
+						}
 						else
 						{
 							echo "<li><a href=\"login.php\"><span class=\"glyphicon glyphicon-log-in\"></span> Sign In</a></li>";
 							echo "<li><a href=\"register.php\"><span class=\"glyphicon glyphicon-user\"></span> Sign Up</a></li>";
 						}
-					?>
-					
+?>
 					</ul>
 				</div>
 			</div>
