1175764f1fdbfff910052514ad93ebbba5ad6393
Martin Bergman Törnkvist <mtt15001@student.mdh.se>
2016-10-12 14:56:29 +0200

    :100: :100: Message design is Lit :100: :100:
    Jag har gjort grundlig design till chatten

---
+++ messages.php
@@ -0,0 +1,31 @@
+<?php include("includes/standard_head.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include("includes/dbconn.php"); ?>
+    </head>
+    <body>
+        <?php include("includes/navbar.php"); ?>
+        
+<div class="container">
+  <div class="row">
+    <nav class="col-sm-3" id="myScrollspy">
+      <ul class="list-group">
+        <li class="list-group-item"><a href="#section1">Friend 1</a></li>
+        <li class="list-group-item"><a href="#section2">Friend 2</a></li>
+        <li class="list-group-item"><a href="#section3">Friend 3</a></li>
+        <li class="list-group-item"><a href="#section41">Friend 4</a></li>
+        <li class="list-group-item"><a href="#section42">Friend 5</a></li>
+          </ul>
+    </nav>
+    <div class="col-sm-9">
+      <div id="section1">
+        <h3>Sender!</h3>
+        <p>Message</p>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
