520886c187644652236d8f71e14791c49db710ee
Enari <Spam@minit.nu>
2016-10-12 22:55:07 +0200

    Moved javascript to footer file

--- includes/standard_head.php
+++ includes/standard_head.php
@@ -1,9 +1,8 @@
+		<!-- Standard head start -->
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="img/favicon.ico">
 		<link href="css/bootstrap.min.css" rel="stylesheet">
 		<link href="css/custom.css" rel="stylesheet">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		
+		<!-- Standard head end -->
