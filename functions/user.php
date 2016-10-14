2fa9ed964a676218167ab3e06e0e89e3d0d8d600
André Caldegren <andweij@gmail.com>
2016-10-14 19:44:07 +0200

    clean

--- functions/user.php
+++ functions/user.php
@@ -17,56 +17,48 @@
 	// Returns the username.
 	function getUsername ($userID)
 	{
-
 		return 'admin';
 	}
 
 	// Returns the user ID.
 	function getUserID ($username)
 	{
-
 		return 1;
 	}
 
 	// Checks if the user is logged in.
-	function loggedIn()
+	function isLoggedIn()
 	{
-
 		return true;
 	}
 
 	// Checks if a user with the given user ID is an admin.
 	function isAdmin ($userID)
 	{
-
 		return fale;
 	}
 
 	// Checks if a user with the given username is an admin.
 	function isAdmin ($username)
 	{
-
 		return true;
 	}
 
 	// Checks if the user with the given user ID is moderator for the forum with the given forum ID.
 	function isModerator ($userID, $forumID)
 	{
-
 		return true;
 	}
 
 	// Checks if the user with the given username is moderator for the forum with the given forum ID.
 	function isModerator ($username, $forumID)
 	{
-
 		return true;
 	}
 
 	// Returns an array of all the forums that the user with the given user ID is moderator for.
 	function isModeratorFor ($userID)
 	{
-
 		return $array;
 	}
 
