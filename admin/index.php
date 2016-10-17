e4ed1b49d3cb97682f4af6d8800be1e1fa52a4f5
André Caldegren <andweij@gmail.com>
2016-10-17 10:18:21 +0200

    fixes

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" href="img/favicon.ico">
 		<link href="../css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/custom.css" rel="stylesheet">
+		<link href="../css/custom.css" rel="stylesheet">
 		<title>Admin</title>
 	</head>
 	<body>
