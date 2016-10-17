72b509af949901c96163d9f40849ad4bb41ee2e4
Martin Bergman Törnkvist <mtt15001@student.mdh.se>
2016-10-17 09:13:07 +0200

    litet

--- messages.php
+++ messages.php
@@ -1,21 +1,21 @@
 <?php
 	session_start();
-	include("includes/dbconn.php"); 
+	require_once("includes/dbconn.php"); 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-<?php include("includes/standard_head.php"); ?>
+<?php require_once("includes/standard_head.php"); ?>
 		<title>Forum- Messages</title>
 	</head>
 	<body>
-<?php include("includes/navbar.php"); ?>
+<?php require_once("includes/navbar.php"); ?>
 		<!-- Content start -->
 		<div class="container">
 			<div class="row">
-				<nav class="col-sm-3" id="myScrollspy">
+				<nav class="col-sm-3">
 					<ul class="list-group">
-						<li class="list-group-item"><a href="#section1">Friend 1</a></li>
+						<li class="list-group-item">Friend 1</a></li>
 						<li class="list-group-item"><a href="#section2">Friend 2</a></li>
 						<li class="list-group-item"><a href="#section3">Friend 3</a></li>
 						<li class="list-group-item"><a href="#section41">Friend 4</a></li>
