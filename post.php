b8432f0bf4d0f3e2731ac39f2f6a479c21ada11f
André Caldegren <andweij@gmail.com>
2016-10-20 10:45:41 +0200

    post page comming along

--- post.php
+++ post.php
@@ -1,5 +1,6 @@
 <?php
 	session_start();
+	require_once "includes/dbconn.php";
 	require_once 'functions/get.php';
 	require_once 'functions/user.php';
 
@@ -16,28 +17,87 @@
 			$forum = false;
 		}
 	}
+	elseif (isset($_GET['id']))
+	{
+		$stmt = $conn->prepare('SELECT * FROM posts WHERE id = ?');
+		$stmt->bind_param('i', $_GET['id']);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		$stmt->store_result();
+		
+
+		if ($result->num_rows == 0)
+			header('Location: index.php');
+
+
+		$post = $result->fetch_array();
+
+		$repliesSTMT = $conn->prepare('SELECT * FROM comments WHERE postID = ?');
+		$repliesSTMT->bind_param('i', $post['id']);
+		$repliesSTMT->execute();
+		$comments = $repliesSTMT->get_result();
+		$repliesSTMT->store_result();
+
+		$repliesSTMT->free_result();
+		$stmt->free_result();
+		$stmt->close();
+		$repliesSTMT->close();
+
+	}
+	else
+	{
+		header('Location: index.php');
+	}
 ?>
 
 <!DOCTYPE html>
 <html>
 	<head>
-<?php include("includes/standard_head.php"); ?>
+		<?php include("includes/standard_head.php"); ?>
 		<title>Forum</title>
 	</head>
 	<body>
 <?php include("includes/navbar.php"); ?>		
 		<!-- Content start -->
 		<div class="container">
-			<h2 class="post-title">Post title</h2>
+			<h2 class="post-title">
+			<?php 
+				$categoryID = forumBelongsTo($post['forum']);
+				echo '<a href="category.php?id='.$categoryID.'">'.getCategoryName($categoryID).'</a> / ';
+				echo '<a href="forum.php?id='.$post['forum'].'">'.getForumName($post['forum']).'</a> / ';
+				echo $post['title']; 
+				?>
+			</h2>
 			<div class="row post-post">
 				<div class="col-lg-2 post-profile">
-					<h4>Username</h4>
+					<h4><?php echo getUsernameID($post['creator']); ?></h4>
 					<img src="img/cat.jpg" alt="Profile picture">
 				</div>
 				<div class="col-lg-10 post-text">
-					<p>asdf</p>
+					<p><?php echo $post['text']; ?></p>
 				</div>
 			</div>
+
+			<?php
+			if ($comments->num_rows > 0)
+			{
+				while ($comment = $comments->fetch_assoc())
+				{
+					echo '<div class="row post-reply">';
+					echo '<div class="col-lg-2 post-profile">';
+					echo '<h4>'.getUsernameID($comment['userID']).'</h4>';
+					echo '<img src="img/cat.jpg" alt="Profile picture">';
+					echo '</div>';
+					echo '<div class="col-lg-10 post-text">';
+					echo '<p>'.$comment['text'].'</p>';
+					echo '</div>';
+					echo '</div>';
+				}
+			}
+
+
+			
+			?>
 			<div class="row post-reply">
 				<div class="col-lg-2 post-profile">
 					<h4>Username</h4>
@@ -47,6 +107,9 @@
 					<p>asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf</p>
 				</div>
 			</div>
+
+
+
 			<div class="row post-reply">
 				<div class="col-lg-2 post-profile">
 					<h4>Username</h4>
