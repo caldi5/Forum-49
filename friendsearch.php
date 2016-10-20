f6052c755b987406f4dbb119a6067acd7dd7d6bf
Enari <Spam@minit.nu>
2016-10-20 10:32:30 +0200

    Friendssearch not useing username2

--- friendsearch.php
+++ friendsearch.php
@@ -2,22 +2,23 @@
 session_start();
 require_once "includes/dbconn.php";
 require_once "functions/user.php";
-$username = getUsername();
+
+$id = getUserID(getUsername());
 if(isset($_POST['search']))
 {
 $searchtext = $_POST['search'];
     
-$result = $conn->query("SELECT username, username2 FROM users
-                        JOIN friends ON users.id = friends.userid
-                        WHERE username = '$username' AND username2 LIKE '%$searchtext%'");
+$result = $conn->query("SELECT username FROM users
+                        JOIN friends ON users.id = friends.userid2
+                        WHERE userid = '$id' AND username LIKE '%$searchtext%'");
 if($result->num_rows > 0)
 {
     while($row = $result->fetch_assoc())
     {
         ?>
-        <a href="#" class="searchoption" onClick="selectFriend('<?php echo $row["username2"]; ?>')">
+        <a href="#" class="searchoption" onClick="selectFriend('<?php echo $row["username"]; ?>')">
         <?php
-        echo $row['username2'];
+        echo $row['username'];
         echo '</a>';
         echo '<br>';
     }
