2361d3851b6f91bd2a124deabb461521187eafdd
André Caldegren <andweij@gmail.com>
2016-11-04 13:23:15 +0100

    favicon

--- includes/standard_head.php
+++ includes/standard_head.php
@@ -2,7 +2,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="icon" href="img/favicon.ico">
+		<link rel="icon" href="img/favicon.png">
 		<link href="/css/bootstrap.min.css" rel="stylesheet">
 		<link href="/css/custom.css" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
