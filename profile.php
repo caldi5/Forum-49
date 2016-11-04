eb69199bef64d60207a43c7b72a36eae8e8b8304
André Caldegren <andweij@gmail.com>
2016-11-04 12:56:35 +0100

    rev

--- profile.php
+++ profile.php
@@ -25,7 +25,19 @@
 	// Need something like this so we don't fail later checks because of capital/normal letters.
 	$user = getUsernameID(getUserID($_GET["user"]));
 
+	$userID = getUserID($_GET["user"]);
 
+	$comments = $conn->prepare('SELECT postID, text, created_at FROM comments WHERE userID = ? ORDER BY created_at DESC LIMIT 10');
+	$comments->bind_param('i', $userID);
+	$comments->execute();
+	$comments->store_result();
+	$comments->bind_result($commentsID, $commentsText, $commentsTime);
+
+	$posts = $conn->prepare('SELECT id, title, created_at FROM posts WHERE creator = ? ORDER BY created_at DESC LIMIT 10');
+	$posts->bind_param('i', $userID);
+	$posts->execute();
+	$posts->store_result();
+	$posts->bind_result($postsID, $postsTitle, $postsTime);
 
 
 ?>
@@ -41,17 +53,12 @@
 		<!-- Content start -->
 		<div class="container">
 			<div class="row">
-		 		<div class="col-md-3">
-					<!-- SIDEBAR USERPIC -->
-					<div class="profile-userpic">
-						<img src="img/testprofilepic.jpg" class="img-responsive" alt="">
-					</div>
-					<!-- END SIDEBAR USERPIC -->
+		 		<div class="col-lg-2">
 					<!-- SIDEBAR USER TITLE -->
 					<div class="profile-usertitle">
 						<div class="profile-usertitle-name">
 						
-												<b><?php echo $user; ?></b>
+												<h1><?php echo $user; ?></h1>
 					
 						</div>
 						<div class="profile-usertitle-userType">
@@ -77,18 +84,13 @@
 								?>
 						
 					<!-- SIDEBAR BUTTONS -->
+
 					<div class="profile-userbuttons">
 												<?php if( !areFriends($currentUser->id, getUserID($user)) ){ 
 												?>
 												
-						 <a type="button" class="btn btn-success btn-sm" id="friendButton" href="#">Add Friend</a>
-											
-												<?php }
-														elseif (requestExists($currentUser->id, $_GET["user"])) { 
-												?>
+						 <a type="button" class="btn btn-success btn-sm" href="#">Add Friend</a>
 												
-														<a type="button" class="btn btn-success btn-sm" href="#">Request Sent</a>
-						
 												<?php }
 														else { 
 												?>
@@ -138,15 +140,65 @@
 				</div>
 		
 				<!-- OVERVIEW CONTENT -->
-				<div id="profile-content-div" class="col-md-9">
-					<div class="profile-content-header">
+				<div class="col-lg-10">
+					<div class="col-lg-5 profile-comments">
+						<h3>Latest Comments</h3>
+						<?php
+							if ($comments->num_rows > 0)
+							{
+								while ($comments->fetch())
+								{
+									echo '<a href="post.php?id='.$commentsID.'">';
+									echo '<div class="col-lg-12 profile-comment">';
+									if (strlen($commentsText) > 50)
+										echo htmlspecialchars(substr($commentsText, 0, 45)).'...';
+									else
+										echo htmlspecialchars($commentsText);
+									echo '<br><span class="post-time">'.date('H:i d/m/y', $commentsTime).'</span>';
+									echo '</div>';
+									echo '</a>';
+								}
 								
-						<h2 id="overview-header-text" align="center">Overview</h2>
-								
+							}
+							else
+							{
+								echo '<div class="col-lg-12 profile-comment">';
+								echo '<p>User has no comments yet.</p>';
+								echo '</div>';
+							}
+							$comments->free_result();
+							$comments->close();
+						?>
 					</div>
-					<div class="profile-content">
-				 		Some user related/created content goes here... 
+
+					<div class="col-lg-5 profile-posts">
+						<h3>Latest Posts</h3>
+						<?php
+							if ($posts->num_rows > 0)
+							{
+								while ($posts->fetch())
+								{
+									echo '<a href="post.php?id='.$postsID.'">';
+									echo '<div class="col-lg-12 profile-comment">';
+									if (strlen($postsTitle) > 50)
+										echo htmlspecialchars(substr($postsTitle, 0, 45)).'...';
+									else
+										echo htmlspecialchars($postsTitle);
+									echo '<br><span class="post-time">'.date('H:i d/m/y', $postsTime).'</span>';
+									echo '</div>';
+									echo '</a>';
+								}
 								
+							}
+							else
+							{
+								echo '<div class="col-lg-12 profile-comment">';
+								echo '<p>User has no posts yet.</p>';
+								echo '</div>';
+							}
+							$posts->free_result();
+							$posts->close();
+						?>
 					</div>
 				</div>
 				<!-- OVERVIEW CONTENT END -->
