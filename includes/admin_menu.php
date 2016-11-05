8a299a8ce56ad532f679f7abff42d32bce085b3a
Enari <Spam@minit.nu>
2016-11-05 09:05:33 +0100

    Admin Menu more responsive!"

--- includes/admin_menu.php
+++ includes/admin_menu.php
@@ -1,10 +1,22 @@
-				<!-- Admin Menu Start--> 
-				<div class="adminMenu">
-					<ul class="nav nav-tabs nav-justified">
-						<li><a href="users.php" class="list-group-item">Users</a></li>
-						<li><a href="categories.php" class="list-group-item">Categories</a></li>
-						<li><a href="forums.php" class="list-group-item">Forums</a></li>
-						<li><a href="#" class="list-group-item">Reports</a></li>
-					</ul>
-				</div>
+				<!-- Admin Menu Start-->
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<label class="navbar-brand">Admin <span class="red-text">Menu</span></label>
+							<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#adminMenu">
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+						</div>
+						<div id="adminMenu" class="collapse navbar-collapse" style="overflow: hidden;">
+							<ul class="nav nav-tabs nav-justified">
+								<li><a href="users.php" class="list-group-item">Users</a></li>
+								<li><a href="categories.php" class="list-group-item">Categories</a></li>
+								<li><a href="forums.php" class="list-group-item">Forums</a></li>
+								<li><a href="#" class="list-group-item">Reports</a></li>
+							</ul>
+						</div>
+					</div>
+				</nav>
 				<!-- Admin Menu End-->
--- js/custom/admin-menu.js
+++ js/custom/admin-menu.js
@@ -2,7 +2,7 @@ $(function()
 {
 		var url = window.location.href;
 		// passes on every "a" tag
-		$(".adminMenu a").each(function() {
+		$("#adminMenu a").each(function() {
 				// checks if its the same on the address bar
 				if (url == (this.href)) {
 						$(this).closest("li").addClass("active");
