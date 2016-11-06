0f10b70974b52b5ca6f9441f343bff785760b92f
Enari <Spam@minit.nu>
2016-11-06 16:37:02 +0100

    New index page

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<?php
-
-	require_once("includes/init.php");
-
-	// Here we get all of the categories that has at least one forum in them.
-	$stmt = $conn->prepare('SELECT id, name 
-		FROM categories 
-		WHERE (SELECT COUNT(id) FROM forums WHERE category = categories.id) > 0
-		ORDER BY ordering');
-	$stmt->execute();
-	$result = $stmt->get_result();
-	$stmt->store_result();
-	$stmt->close();
-	
-?>
+<?php	require_once("includes/init.php"); ?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -20,69 +6,46 @@
 		<title>Forum</title>
 	</head>
 	<body>
-
-		<?php include("includes/navbar.php"); ?>
+<?php include("includes/navbar.php"); ?>
 		<!-- Content start -->
 		<div class="container">
 <?php	displayAlerts(); ?>
-		<?php
-			if ($result->num_rows > 0)
+<?php
+
+			$categories = getCategories();
+			foreach ($categories as $category) 
 			{
-				// Loops thru all of the selected forums.
-				while ($row = $result->fetch_assoc())
+				$forums = $category->getForums();
+
+				echo '<div class="row category">';
+				echo '<h3 class="category-title"><a href="category.php?id='.$category->id.'">'.$category->name.'</a></h3>';
+				
+				$i = 0; //To limit number of forums displayed to 3
+				foreach($forums as $forum)
 				{
-					echo '<div class="row category">';
-					echo '<h3 class="category-title"><a href="category.php?id='.$row['id'].'">'.$row['name'].'</a></h3>';
-
-					// Here we get the forums that belongs to the current category.
-					$stmt = $conn->prepare('SELECT * FROM forums WHERE category = ? ORDER BY ordering LIMIT 3');
-					$stmt->bind_param('i', $row['id']);
-					$stmt->execute();
-					$result_2 = $stmt->get_result();
-					$stmt->store_result();
-					$stmt->close();
-
-					// Just an extra safety check, this should never be false. Explained below.
-					if($result_2->num_rows > 0)
-					{
-						// Lopps thru the forums for this category.
-						while ($forum = $result_2->fetch_assoc())
-						{
-							//Anton test code for objects start
-							$forumObject = new forum($forum['id']);
-							//Anton test code for objects END
-							echo '<a href="forum.php?id='.$forum['id'].'">';
-							echo '<div class="col-lg-12 forum">';
-							echo '<div class="col-lg-10">';
-							echo '<h4 class="forum-title">'.htmlspecialchars($forum['name']).'</h4>';
-							echo '<p class="forum-desc">'.htmlspecialchars($forum['description']).'</p>';
-							echo '</div>';
-							echo '<div class="col-lg-1">';
-							echo '<p>Posts:<br> '.numberOfPosts($forum['id']).'</p>';
-							echo '</div>';
-							echo '<div class="col-lg-1">';
-							echo '<p>Views:<br>'. $forumObject->getNumberOfviews() .'</p>';
-							echo '</div>';
-							echo '</div>';
-							echo '</a>';
-						}
-					}
-					else
-					{
-						// This should not be able to happen since we only select thoose categories with at least one forum.
-						echo 'This should not be possible.';
-						die();
-					}
+					echo '<a href="forum.php?id='.$forum->id.'">';
+					echo '<div class="col-lg-12 forum">';
+					echo '<div class="col-lg-10">';
+					echo '<h4 class="forum-title">'.htmlspecialchars($forum->name).'</h4>';
+					echo '<p class="forum-desc">'.htmlspecialchars($forum->description).'</p>';
 					echo '</div>';
-				}
-				$result_2->free_result();
-				$result->free_result();
+					echo '<div class="col-lg-1">';
+					echo '<p>Posts:<br> '.$forum->getNumberOfPosts().'</p>';
+					echo '</div>';
+					echo '<div class="col-lg-1">';
+					echo '<p>Views:<br>'. $forum->getNumberOfviews() .'</p>';
+					echo '</div>';
+					echo '</div>';
+					echo '</a>';
+					if (++$i == 3) break;
+				}				
+				echo '</div>';
 			}
-			else
+			if(empty($categories))
 			{
 				echo 'No categories? Something is wrong here.';
 			}
-		?>
+?>
 		</div>
 		<?php require_once("includes/standard_footer.php"); ?>
 	</body>
