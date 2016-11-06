6a9257003c1569fa18331d0a30d49105b0ce4172
Enari <Spam@minit.nu>
2016-11-06 17:19:52 +0100

    ajax send message echo

--- ajax/sendMessage.php
+++ ajax/sendMessage.php
@@ -3,6 +3,6 @@
 	require_once("../includes/init.php");
 
 	if(isset($_GET['to']) && isset($_GET['message']))
-		$currentUser->sendMessage($_GET['to'], $_GET['message']);
+		echo $currentUser->sendMessage($_GET['to'], $_GET['message']);
 
 	?>
