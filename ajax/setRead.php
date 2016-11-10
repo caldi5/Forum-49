fe39eecbcde98ffb583fced2957d5ea0e43da40d
André Caldegren <andweij@gmail.com>
2016-11-10 10:19:01 +0100

    fix

--- ajax/setRead.php
+++ ajax/setRead.php
@@ -12,7 +12,7 @@
 
 	// The SQL to update the isread column
 	$stmt = $conn->prepare("UPDATE messages SET isread = 1 WHERE to_user = ? and from_user = ?");
-	$stmt->bind_param('iiii', $id, $partner);
+	$stmt->bind_param('ii', $id, $partner);
 	$stmt->execute();
 	$stmt->close();
 
