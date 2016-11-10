d5daaf5da3be0fe8a728aee36d0e08feb7c02f74
William Achrenius <willach@home.se>
2016-11-10 13:04:26 +0100

    fixxx

--- profile.php
+++ profile.php
@@ -105,7 +105,7 @@
 											<?php }
 											?>
 												
-					 <a type="button" class="btn btn-danger btn-sm" href="messages.php?user=<?php echo $user->username;?>">Message</a>
+					 <a type="button" class="btn btn-primary btn-sm" href="/messages.php">Message</a>
 					</div>
 					<!-- END SIDEBAR BUTTONS -->    
 						 
