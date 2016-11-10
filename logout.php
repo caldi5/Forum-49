f533b27ae7f4413045dbdfdf8069d7d3379edac5
André Caldegren <andweij@gmail.com>
2016-11-10 10:39:49 +0100

    cookie delete fix

--- logout.php
+++ logout.php
@@ -1,21 +1,15 @@
-<script type="text/javascript">
-	
-	$.removeCookie('cn');
-	$.removeCookie('c0');
-	$.removeCookie('c1');
-	$.removeCookie('c2');
-	$.removeCookie('c3');
-
-
-</script>
-
-
-
 <?php
 
 	session_start();
+
 	if(session_destroy())
 	{
+		setcookie("cn", "", time()-3600);
+		setcookie("c0", "", time()-3600);
+		setcookie("c1", "", time()-3600);
+		setcookie("c2", "", time()-3600);
+		setcookie("c3", "", time()-3600);
+		
 		header("location: index.php");
 	}
 ?>
