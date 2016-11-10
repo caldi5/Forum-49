cb42266b916d179bb0bf4413234d49adcb77b4a3
André Caldegren <andweij@gmail.com>
2016-11-10 10:36:56 +0100

    cookie fix

--- logout.php
+++ logout.php
@@ -1,4 +1,18 @@
+<script type="text/javascript">
+	
+	$.removeCookie('cn');
+	$.removeCookie('c0');
+	$.removeCookie('c1');
+	$.removeCookie('c2');
+	$.removeCookie('c3');
+
+
+</script>
+
+
+
 <?php
+
 	session_start();
 	if(session_destroy())
 	{
